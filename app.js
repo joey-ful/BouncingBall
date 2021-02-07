891c5808f504ca5f826302e5e5430dbfc21919f6
jehong <jehong@student.42seoul.com>
2021-02-07 23:19:55 +0900

    add bouncing logo balls animation

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-import {Ball} from './ball.js';
+import Ball from './ball.js';
+import {logos} from './consts.js';
 
 class App {
   constructor() {
     this.canvas = document.createElement('canvas');
+    this.canvas.setAttribute('id', 'canvas');
     this.ctx = this.canvas.getContext('2d');
 
     document.body.appendChild(this.canvas);
@@ -10,7 +12,9 @@ class App {
     window.addEventListener('resize', this.resize.bind(this), false);
     this.resize();
 
-    this.ball = new Ball(200, 200, 30, 15);
+    this.balls = [];
+    this.createBall();
+    this.slowDown();
     this.animate();
   }
 
@@ -24,10 +28,50 @@ class App {
     this.ctx.scale(2, 2);
   }
 
+  slowDown() {
+    this.canvas.addEventListener('mouseover', () => {
+      this.balls.forEach((ball) => {
+        ball.vx /= 20;
+        ball.vy /= 20;
+      });
+    });
+
+    this.canvas.addEventListener('mouseout', () => {
+      this.balls.forEach((ball) => {
+        ball.vx *= 20;
+        ball.vy *= 20;
+      });
+    });
+  }
+
+  createBall() {
+    for (let i = 0; i < logos.length; i++) {
+      let radius = Math.ceil(Math.random() * 30) + 10;
+      let speedX = Math.ceil(Math.random() * 50) + 5;
+      let speedY = Math.ceil(Math.random() * 50) + 5;
+      let signX = speedX % 2 === 0 ? -1 : 1;
+      let signY = speedY % 2 === 0 ? -1 : 1;
+
+      this.balls.push(
+        new Ball(
+          this.stageWidth,
+          this.stageHeight,
+          radius,
+          speedX * signX,
+          speedY * signY,
+          `./srcs/${logos[i]}.png`
+        )
+      );
+    }
+  }
+
   animate() {
     window.requestAnimationFrame(this.animate.bind(this));
     this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
-    this.ball.draw(this.ctx, 'red', this.stageWidth, this.stageHeight);
+
+    this.balls.forEach((ball) => {
+      ball.draw(this.ctx, this.stageWidth, this.stageHeight);
+    });
   }
 }
 
--- ball.js
+++ ball.js
@@ -1,22 +1,47 @@
-export class Ball {
-  constructor(x, y, radius, speed) {
+export default class Ball {
+  constructor(stageWidth, stageHeight, radius, speedX, speedY, src) {
+    this.img = new Image();
     this.radius = radius;
-    this.vx = speed;
-    this.vy = speed;
-    this.x = x;
-    this.y = y;
+    this.diameter = this.radius * 2;
+    this.speedX = speedX;
+    this.speedY = speedY;
+    this.vx_minus = this.speedX < 0 ? -1 : 1;
+    this.vy_minus = this.speedY < 0 ? -1 : 1;
+    this.vx = this.speedX;
+    this.vy = this.speedY;
+    this.src = src;
+
+    this.x = this.radius + Math.random() * (stageWidth - this.diameter);
+    this.y = this.radius + Math.random() * (stageHeight - this.diameter);
   }
 
-  draw(ctx, color, canvasWidth, canvasHeight) {
+  draw(ctx, stageWidth, stageHeight) {
+    this.interaction();
     this.x += this.vx;
     this.y += this.vy;
 
-    this.bounceWindow(canvasWidth, canvasHeight);
+    this.bounceWindow(stageWidth, stageHeight);
 
+    ctx.save();
     ctx.beginPath();
+    ctx.fillStyle = 'white';
+    ctx.shadowColor = '#dee2e6';
+    ctx.shadowBlur = 10;
+    ctx.shadowOffsetX = 5;
+    ctx.shadowOffsetY = 3;
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-    ctx.fillStyle = color;
     ctx.fill();
+    ctx.closePath();
+    ctx.clip();
+    ctx.drawImage(
+      this.img,
+      this.x - this.radius,
+      this.y - this.radius,
+      this.diameter,
+      this.diameter
+    );
+    this.img.src = this.src;
+    ctx.restore();
   }
 
   bounceWindow(stageWidth, stageHeight) {
@@ -29,4 +54,73 @@ export class Ball {
       this.y += this.vy;
     }
   }
+
+  interaction() {
+    this.canvas = document.getElementById('canvas');
+
+    this.canvas.addEventListener('mousedown', this.onMouseDown);
+    this.canvas.addEventListener('mouseup', this.onMouseUp);
+    this.canvas.addEventListener('mouseout', this.onMouseOut);
+  }
+
+  setMinus = () => {
+    this.vx_minus = this.vx < 0 ? -1 : 1;
+    this.vy_minus = this.vy < 0 ? -1 : 1;
+  };
+
+  applyMinus = () => {
+    if (this.vx_minus * this.speedX < 0) {
+      this.vx *= -1;
+    }
+    if (this.vy_minus * this.speedY < 0) {
+      this.vy *= -1;
+    }
+  };
+
+  onMouseDown = (e) => {
+    this.setMinus();
+
+    this.offsetX = e.clientX - this.x;
+    this.offsetY = e.clientY - this.y;
+
+    if (
+      Math.abs(this.offsetX) <= this.radius &&
+      Math.abs(this.offsetY) <= this.radius
+    ) {
+      this.canvas.addEventListener('mousemove', this.onMouseMove);
+    }
+  };
+
+  onMouseMove = (e) => {
+    this.x = e.clientX - this.offsetX;
+    this.y = e.clientY - this.offsetY;
+
+    this.vx = 0;
+    this.vy = 0;
+    this.draw(
+      this.canvas.getContext(
+        '2d',
+        this.canvas.width / 2,
+        this.canvas.height / 2
+      )
+    );
+  };
+
+  onMouseUp = (e) => {
+    this.vx = this.speedX / 20;
+    this.vy = this.speedY / 20;
+    this.applyMinus();
+
+    this.canvas.removeEventListener('mousedown', this.onMouseDown);
+    this.canvas.removeEventListener('mousemove', this.onMouseMove);
+  };
+
+  onMouseOut = (e) => {
+    this.vx = this.speedX;
+    this.vy = this.speedY;
+    this.applyMinus();
+
+    this.canvas.removeEventListener('mousedown', this.onMouseDown);
+    this.canvas.removeEventListener('mousemove', this.onMouseMove);
+  };
 }
